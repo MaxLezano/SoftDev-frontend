bb159c156c52d19a1fe951efc4f7cc54048f08cf
unknown <Dan1el-aguilar>
2022-07-31 18:08:19 -0300

    feature/create_component-container_card

---
+++ src/components/sectionProducts/SectionProducts.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Card from '../Card/Card'
+import './SectionProducts.css'
+
+const SectionProducts = () => {
+  return (
+    <div className='grid-container'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+     
+    </div>
+    
+  )
+}
+
+export default SectionProducts
